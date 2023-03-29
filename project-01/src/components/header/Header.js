7e3d4281699b062d713a988bfb61e8dc38655eb2
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-29 23:49:28 +0530

    add content

--- project-01/src/components/header/Header.js
+++ project-01/src/components/header/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <div className="bg-accent-tes-200 h-13 w-full text-black fixed top-0 p-4">
+      <div className="bg-green-500 border-gray-200 px-4 h-16  dark:bg-gray-600 font-Poppins text-white">
         <span>this is header my</span>
       </div>
     )
