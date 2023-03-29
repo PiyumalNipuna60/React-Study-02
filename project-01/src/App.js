5ff60751ded0c199f1d6031d5a2aafe1bbc85888
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-29 21:02:23 +0530

    Crate fitst component

--- project-01/src/App.js
+++ project-01/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import PartOne from './components/PartOne';
 
 function App() {
   return (
     <div className="App">
       <h1>This is my first project</h1>
+      <PartOne />
     </div>
   );
 }
---
+++ project-01/src/components/PartOne.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PartOne extends Component {
+  render() {
+    return (
+      <div>
+        <h2>this is component</h2>
+      </div>
+    )
+  }
+}
