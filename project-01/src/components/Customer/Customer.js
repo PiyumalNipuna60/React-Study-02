35edacb0181231d3e9eba08cb943a4e64ab401a5
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-12 23:01:14 +0530

    setup 01 : create props

--- project-01/src/components/Customer/Customer.js
+++ project-01/src/components/Customer/Customer.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 
-export default class Customer extends Component {
+type CustomerProps = {
+    id:String;
+    name:String;
+    address:String;
+    salary:String;
+};
+
+export default class Customer extends Component <CustomerProps>{
   render() {
     return (
       <div className='bg-gray-300 w-1/3 h-64 p-2 flex flex-col justify-center items-center m-3 rounded-2xl'>
-        <h1>C001</h1>
-        <h1>kamal</h1>
-        <h1>matara</h1>
-        <h1>25000</h1>
+        <h1>{this.props.id}</h1>
+        <h1>{this.props.name}</h1>
+        <h1>{this.props.address}</h1>
+        <h1>{this.props.salary}</h1>
       </div>
     )
   }
