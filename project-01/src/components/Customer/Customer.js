f82cf348fef29915f35753fe737178809391763a
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-12 21:27:45 +0530

    justify and item center

--- project-01/src/components/Customer/Customer.js
+++ project-01/src/components/Customer/Customer.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react'
 export default class Customer extends Component {
   render() {
     return (
-      <div>Customer</div>
+      <div className='bg-gray-300 w-2/3 h-64 p-2 flex justify-center items-center' >
+        <h1>C001</h1>
+        <h1>kamal</h1>
+        <h1>matara</h1>
+        <h1>25000</h1>
+      </div>
     )
   }
 }
