b17f4a5d68c57e23090b57f47cd96254574b831a
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-12 23:04:43 +0530

    setup 02 : pass props data

--- project-01/src/components/Content/Content.js
+++ project-01/src/components/Content/Content.js
@@ -5,10 +5,38 @@ export default class Cuntent extends Component {
   render() {
     return (
       <div className='flex'>
-        <Customer />
-        <Customer />
-        <Customer />
-        <Customer />
+        <Customer {...{
+          id:"C001",
+          name:"kamal",
+          address:"galle",
+          salary:"25000"
+          
+         }}/>
+
+        <Customer {...{
+          id:"C002",
+          name:"sadun",
+          address:"galle",
+          salary:"25000"
+          
+         }}/>
+
+        <Customer {...{
+          id:"C003",
+          name:"charith",
+          address:"galle",
+          salary:"25000"
+          
+         }}/>
+
+        <Customer {...{
+          id:"C004",
+          name:"nipuna",
+          address:"galle",
+          salary:"25000"
+          
+         }}/>
+        
       </div>
     )
   }
