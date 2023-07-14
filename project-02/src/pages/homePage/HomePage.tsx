79f8c529bca8742063980bad858871e54e131c9d
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-07-14 21:11:04 +0530

    Add Student compononet

--- project-02/src/pages/homePage/HomePage.tsx
+++ project-02/src/pages/homePage/HomePage.tsx
@@ -1,22 +1,13 @@
-import { Paper } from '@mui/material'
 import React, { Component } from 'react'
+import Student from '../../components/student/Student'
 
 export default class HomePage extends Component {
   render() {
     return (
-      <div className='flex m-auto p-5 justify-center'>
-        <Paper elevation={3} className='p-10 text-center'>
-          <h3 className='text-4xl font-semibold'>S001</h3>
-          <h2 className='text-3xl'>Piyumal</h2>
-          <p className='text-2xl'>matara</p>
-        <div className="flex mt-2 gap-4 justify-center">
-          <span className='p-3 border rounded'>Maths</span>
-          <span className='p-3 border rounded'>Science</span>
-          <span className='p-3 border rounded'>Sinhala</span>
-        </div>
-        </Paper>
-
-        
+      <div className='flex m-auto p-5 justify-center gap-3'>
+        <Student />
+        <Student />
+        <Student />
       </div>
     )
   }
