4beee70e6e5754964da861068aff2992333bb661
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-07-16 16:01:22 +0530

    get  back end data and catch homepage

--- project-02/src/pages/homePage/HomePage.tsx
+++ project-02/src/pages/homePage/HomePage.tsx
@@ -1,7 +1,26 @@
 import React, { Component } from 'react'
 import Student from '../../components/student/Student'
 
-export default class HomePage extends Component {
+type StudentDetails={
+  id:String;
+  name:String;
+  address:String;
+  subject:String[];
+};
+
+type HomeProps={
+}
+
+type HomeState={
+  students:StudentDetails[];
+}
+
+export default class HomePage extends Component<HomeProps,HomeState> {
+constructor(props:HomeProps){
+ super(props);
+ this.state={students:[{id:"S001",name:"nipuna",address:"galle",subject:["S1","S2","S3"]}]}
+}
+
   render() {
     return (
       <div className='flex m-auto p-5 justify-center gap-3'>
