0bf35a1e5f20c6c429421863da699b4093f1ed14
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-07-16 17:05:23 +0530

    nav bar Active page disaign

--- project-02/src/components/header/Header.tsx
+++ project-02/src/components/header/Header.tsx
@@ -4,6 +4,9 @@ import { NavLink } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
+
+    const navStyle="text-red-700  font-bold";
+
     return (
       <div className="bg-green-400 border-gray-200 px-4 h-16  dark:bg-gray-600 font-Poppins text-white  rounded-xl">
         <div className="w-full h-full drop-shadow-xl">
@@ -13,30 +16,28 @@ export default class Header extends Component {
           </div>
           <div className=' w-2/4 '>
             <nav className='flex gap-3 space-x-2 w-full justify-between'>
-            <NavLink to={"/"}>
+            <NavLink to={"/"} className={({isActive})=>(isActive ? navStyle : "")}>
               Home
             </NavLink>
 
-            <NavLink to={"/about"}>
+            <NavLink to={"/about"} className={({isActive})=>(isActive ? navStyle : "")}>
               About
             </NavLink>
 
-            <NavLink to={"/profile"}>
+            <NavLink to={"/profile"} className={({isActive})=>(isActive ? navStyle : "")}>
               Profile
             </NavLink>
 
-            <NavLink to={"contact"}>
+            <NavLink to={"contact"}  className={({isActive})=>(isActive ? navStyle : "")}>
                Contact
             </NavLink>
 
-            <NavLink to={"/login"}><ExitToAppIcon />
+            <NavLink to={"/login"}  className={({isActive})=>(isActive ? navStyle : "")}><ExitToAppIcon />
             </NavLink>
             </nav>
           </div>
-       
         </div>
-        </div>
-        
+        </div>  
       </div>
     )
   }
